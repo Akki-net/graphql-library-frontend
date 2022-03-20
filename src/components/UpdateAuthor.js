78bab9b8342cec23e54b4ae888b836f2cb081d2d
unknown <akashsharma.zak@gmail.com>
2022-03-20 21:52:32 +0530

    Change the birth year form so that a birth year can be set only for an existing author

--- src/components/UpdateAuthor.js
+++ src/components/UpdateAuthor.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
-import { useMutation } from '@apollo/client'
+import { useMutation, useQuery } from '@apollo/client'
 import { SET_BIRTH, GET_AUTHORS } from './queries'
 
 const UpdateAuthor = () => {
     const [name, setName] = useState('')
     const [birth, setBirth] = useState('')
+    const fetchAuth = useQuery(GET_AUTHORS)
     const [setAuthorBirth] = useMutation(SET_BIRTH, {
         refetchQueries: [{ query: GET_AUTHORS }]
     })
+
+    if (fetchAuth.loading) {
+        return <div>...loading</div>
+    }
 
     const submit = event => {
         event.preventDefault()
@@ -22,7 +27,12 @@ const UpdateAuthor = () => {
         <div>
             <h3>Set birthyear</h3>
             <form onSubmit={submit}>
-                <input value={name} onChange={({ target }) => setName(target.value)} /><br />
+                <select value={name} onChange={({ target }) => setName(target.value)}>
+                    {fetchAuth.data.allAuthors.map(author => <option value={author.name}>
+                        {author.name}
+                    </option>)}
+                </select><br/>
+                {/* <input value={name} onChange={({ target }) => setName(target.value)} /><br /> */}
                 <input value={birth} maxLength="4" onChange={({ target }) => !isNaN(target.value) && setBirth(target.value)} /><br />
                 <button type="submit">update author</button>
             </form>
